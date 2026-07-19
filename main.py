bd79c5b073d8b75b33b024e58d19362a7dd4f3e4
Ahsan Sajjad <asajjad827@gmail.com>
2026-07-19 17:37:25 +0500

    Stage 1: Root and Health Endpoints

---
+++ main.py
@@ -0,0 +1,25 @@
+from fastapi import FastAPI
+
+app = FastAPI(
+    title="Task API",
+    description="A simple CRUD API for managing tasks.",
+    version="1.0"
+)
+
+
+@app.get("/")
+def root():
+    return {
+        "name": "Task API",
+        "version": "1.0",
+        "endpoints": [
+            "/tasks"
+        ]
+    }
+
+
+@app.get("/health")
+def health():
+    return {
+        "status": "ok"
+    }
